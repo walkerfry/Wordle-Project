7e73f81e206385bec95c3f179c5ef100b4f040ce
Walker Fry <walker.fry@students.fhu.edu>
2026-04-03 23:00:47 -0500

    Fixed wordle guessing logic, seperated correct/misplaced letter checks, stopped invalid words from counting as a guess.

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ class Game:
     def __init__(self, player_name):
         self.player = Player(player_name)
         self.word_bank = WordChoice()
-        self.secret_word = self.word_bank.random_word()
+        self.secret_word = self.word_bank.random_word().lower()
         self.max_guesses = 6
         self.attempts = 0
 
@@ -29,6 +29,12 @@ class Game:
         while not self.gameover():
             guess = input(f"Enter guess ({self.attempts+1}/6): ").lower()
 
+            feedback = self.guess_checker(guess)
+
+            if "Invalid" in feedback:
+                print(feedback)
+                continue
+
             self.player.submit_guess(guess)
             self.attempts += 1
 
@@ -36,25 +42,28 @@ class Game:
                 print(f"You Win! {self.secret_word} guessed in {self.attempts} attempts")
                 return
             
-            self.result(self.guess_checker(guess))
+            self.result(feedback)
         
         print("Game Over! Word was:", self.secret_word)
 
     def guess_checker(self, guess):
-        result = ""
+        result = ["_"] * 5
+        secret = list(self.secret_word)
 
         if len(guess) != 5 or not guess.isalpha():
             return "Invalid Guess: guess must be a 5 letter word"
-
-        for i in range(5):
-            if guess[i] == self.secret_word[i]:
-                result += guess[i].upper()
-            elif guess[i] in self.secret_word:
-                result += guess[i]
-            else:
-                result += "_"
         
-        return result
+        for i in range(5):
+            if guess[i] == secret[i]:
+                result[i] = guess[i].upper()
+                secret[i] = None
+            
+        for i in range(5):
+            if result[i] == "_" and guess[i] in secret:
+                result[i] = guess[i]
+                secret[secret.index(guess[i])] = None
+        
+        return "".join(result)
     
     def result(self, feedback):
         print("Result:", feedback)
@@ -79,7 +88,8 @@ class WordChoice():
         self.word_list.append(word)
 
     def remove_word(self, word):
-        self.word_list.remove(word)
+        if word in self.word_list:
+            self.word_list.remove(word)
 
     def check_word(self, word):
         return word in self.word_list
